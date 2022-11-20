12e0d9e5b32c7a5ca67680e09bdd1cfd259de4ec
Nikolajker <Mr.venik2004@mail.ru>
2022-11-20 18:07:10 +0300

    add number1.1

---
+++ Number1_1/Number1_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Number1_1
+{
+    class Number1_1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+                for (int j = i + 1; j < 10; j++)
+                    for (int k = j + 1; k < 10; k++)
+                        for (int l = k + 1; l < 10; l++)
+                            Console.WriteLine(i.ToString()+j.ToString()+k.ToString()+l.ToString());
+        }
+    }
+}
