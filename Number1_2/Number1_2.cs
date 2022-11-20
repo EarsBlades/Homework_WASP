200217057b27afdac83ca483d7844349efef354d
Nikolajker <Mr.venik2004@mail.ru>
2022-11-20 16:40:27 +0300

    test

---
+++ Number1_2/Number1_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Number1_2
+{
+    class Number1_2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(Math.Abs(i-j) + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Number1_4/Number1_4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Number1_4
+{
+    class Number1_4
+    { static int fact(int n)
+        {
+            int x = 1;
+            for (int i = 1; i <= n; i++) 
+                x *= i;
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту: "); 
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                    Console.Write(fact(i)/(fact(j) * fact(i - j)) + " ");
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
+
